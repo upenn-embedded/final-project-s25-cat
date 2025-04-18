6237640d7bbea1a160e1e326eab91e271d273537
27tjammula <tjammula@seas.upenn.edu>
2025-04-18 11:23:48 -0400

    code

--- code/spi.c
+++ code/spi.c
@@ -20,32 +20,34 @@ void spiInit() {
     DDRB |= (1 << MISO); // MISO is output
 }
 
-char SPI_Recv(void) {
+char SPI_Recv(uint8_t dist) {
+    SPDR0 = dist;
     while(!(SPSR0 & (1 << SPIF)));
     char message = SPDR0;
-    uint8_t response = getDistance(); // preload distance
+//    uint8_t response = getDistance(); // preload distance
     // TODO: tbh this might be fine, esp with fast sampling, but might have to preload for real
     
     switch (message) {
         case 'F':
-            response = 1;
+//            response = 1;
             break;
         case 'R':
-            response = 2;
+//            response = 2;
             break;
         case 'B':
-            response = 3;
+//            response = 3;
             break;
         case 'L':
-            response = 4;
+//            response = 4;
             break;
         case 'D':
-            response = 5;
+//            response = 5;
             break;
         case 'S':
             break;
     }
     
-    SPDR0 = response;
+//    SPDR0 = response;
+    printf("Message: %c", message);
     return message;
 }
